dd63d4d2395901aac8adfdff00880d2b9fd22f99
feigebabata <feigebabata@outlook.com>
2020-03-07 23:34:51 +0800

    sadsa

---
+++ QuadFullCamera/QuadFullCamera.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FG
+{
+	[RequireComponent(typeof(Camera))]
+	public class QuadFullCamera : MonoBehaviour 
+	{
+		Camera m_camera;
+		int m_width,m_height;
+		bool m_orthographic;
+		float m_orthographicSize;
+		float m_fieldOfView;
+		public QuadFullCameraChild[] m_Quads;
+
+
+		// Use this for initialization
+		void Awake () 
+		{
+			m_camera = GetComponent<Camera>();
+			resetLastData();
+			resetQuadT();
+		}
+
+		// void Start()
+		// {
+			
+		// }
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			if(m_Quads!=null && m_Quads.Length>0)
+			{
+				if(m_camera.pixelWidth != m_width || 
+				m_camera.pixelHeight != m_height ||
+				m_camera.orthographic != m_orthographic ||
+				m_camera.orthographicSize != m_orthographicSize ||
+				m_camera.fieldOfView != m_fieldOfView
+				)
+				{
+					resetLastData();
+					resetQuadT();
+				}
+			}
+		}
+
+		void resetLastData()
+		{
+			m_width = m_camera.pixelWidth;
+			m_height = m_camera.pixelHeight;
+			m_orthographic = m_camera.orthographic;
+			m_orthographicSize = m_camera.orthographicSize;
+			m_fieldOfView = m_camera.fieldOfView;
+
+		}
+
+		void resetQuadT()
+		{
+			if(m_orthographic)
+			{
+				for (int i = 0; i < m_Quads.Length; i++)
+				{
+					Vector2 scale = m_Quads[i].transform.localScale;
+					scale.y = m_orthographicSize*2;
+					scale.x = scale.y*m_width/m_height;
+					m_Quads[i].transform.localScale = scale;
+					m_Quads[i].SetCrop(m_width,m_height);
+				}
+			}
+			else
+			{
+				float offset = 0;
+				float tan = Mathf.Tan(Mathf.Deg2Rad*m_fieldOfView/2);
+				for (int i = 0; i < m_Quads.Length; i++)
+				{
+					offset = m_Quads[i].transform.localPosition.z;
+					Vector2 scale = m_Quads[i].transform.localScale;
+					scale.y = offset*tan*2;
+					scale.x = scale.y*m_width/m_height;
+					m_Quads[i].transform.localScale = scale;
+					m_Quads[i].SetCrop(m_width,m_height);
+				}
+			}
+		}
+	}
+}
---
+++ QuadFullCamera/QuadFullCameraChild.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FG
+{
+	[RequireComponent(typeof(MeshRenderer))]
+	public class QuadFullCameraChild : MonoBehaviour 
+	{
+		public string[] TexNames=new string[]{"_MainTex"};
+		Material m_mat;
+		void Awake()
+		{
+			m_mat = GetComponent<MeshRenderer>().material;
+		}
+
+		public void SetCrop(int _width,int _height)
+		{
+			if(TexNames!=null && TexNames.Length>0)
+			{
+				for (int i = 0; i < TexNames.Length; i++)
+				{
+					if(m_mat.GetTexture(TexNames[i]))
+					{
+						setTexCrop(TexNames[i],_width,_height);
+					}
+					else
+					{
+						Debug.LogWarningFormat("[QuadFullCameraChild.SetCrop]{0}无贴图:{1}",name,TexNames[i]);
+					}
+				}
+			}
+		}
+
+        void setTexCrop(string _name,int _width,int _height)
+        {
+            Texture tex = m_mat.GetTexture(_name);
+            Vector2 size = new Vector2(tex.width, tex.height);
+            Vector2 viewSize = new Vector2(_width,_height);
+            if(viewSize==Vector2.zero)
+            {
+                viewSize = new Vector2(Screen.width,Screen.height);
+            }
+            Vector2 tiling = Vector2.one;
+            Vector2 offset = Vector2.zero;
+            if (size.x / size.y > viewSize.x / viewSize.y)
+            {
+                size /= size.y;
+                viewSize /= viewSize.y;
+                tiling.x = viewSize.x / size.x;
+                offset.x = (1 - tiling.x) / 2;
+            }
+            else
+            {
+                size /= size.x;
+                viewSize /= viewSize.x;
+                tiling.y = viewSize.y / size.y;
+                offset.y = (1 - tiling.y) / 2;
+            }
+            m_mat.SetTextureScale(_name, tiling);
+            m_mat.SetTextureOffset(_name, offset);
+        }
+	}
+}
