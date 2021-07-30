d712fcf1d56121d43f92e9c62919ff82ab7cffd9
飞鸽巴巴塔 <feigebabata@outlook.com>
2021-07-30 12:46:25 +0800

    Update MonoSingleton.cs
    修改mono单例

--- singleton/Scripts/MonoSingleton.cs
+++ singleton/Scripts/MonoSingleton.cs
@@ -1,71 +1,74 @@
 using UnityEngine;
 
-public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
-{
-	private static T mInstance = null;
+// namespace FGUFW.Core
+// {
+	public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+	{
+		private static T mInstance = null;
 
-	public static T Instance
-    {
-        get
-        {
+		public static T I
+		{
+			get
+			{
+				if (mInstance == null)
+				{
+					mInstance = GameObject.FindObjectOfType(typeof(T)) as T;
+					if (mInstance == null)
+					{
+						GameObject go = new GameObject(typeof(T).Name);
+						mInstance = go.AddComponent<T>();
+					}
+				}
+
+				return mInstance;
+			}
+		}
+
+
+		private void Awake()
+		{
 			if (mInstance == null)
-            {
-            	mInstance = GameObject.FindObjectOfType(typeof(T)) as T;
-                if (mInstance == null)
-                {
-                    GameObject go = new GameObject(typeof(T).Name);
-                    mInstance = go.AddComponent<T>();
-                    GameObject parent = GameObject.Find("Boot");
-                    if (parent == null)
-                    {
-                        parent = new GameObject("Boot");
-                        GameObject.DontDestroyOnLoad(parent);
-                    }
-                    if (parent != null)
-                    {
-                        go.transform.parent = parent.transform;
-                    }
-                }
-            }
+			{
+				mInstance = this as T;
 
-            return mInstance;
-        }
-    }
+				if(IsDontDestroyOnLoad())
+				{
+					DontDestroyOnLoad(gameObject);
+				}
 
-    /*
-     * 没有任何实现的函数，用于保证MonoSingleton在使用前已创建
-     */
-    public void Startup()
-    {
+				Init();
+			}
+			else
+			{
+				Debug.LogError("mono鍗曚緥閲嶅");
+			}
+		}
 
-    }
+		/// <summary>
+		/// This function is called when the MonoBehaviour will be destroyed.
+		/// </summary>
+		void OnDestroy()
+		{
+			Dispose();
+			MonoSingleton<T>.mInstance = null;
+		}
+	
+		protected virtual void Init()
+		{
 
-    private void Awake()
-    {
-        if (mInstance == null)
-        {
-            mInstance = this as T;
-        }
+		}
 
-        DontDestroyOnLoad(gameObject);
-        Init();
-    }
- 
-    protected virtual void Init()
-    {
+		public void DestroySelf()
+		{
+			UnityEngine.Object.Destroy(gameObject);
+		}
 
-    }
+		public virtual void Dispose()
+		{
 
-    public void DestroySelf()
-    {
-        Dispose();
-        MonoSingleton<T>.mInstance = null;
-        UnityEngine.Object.Destroy(gameObject);
-    }
+		}
 
-    public virtual void Dispose()
-    {
+		protected abstract bool IsDontDestroyOnLoad();
 
-    }
-
-}
+	}
+// }
